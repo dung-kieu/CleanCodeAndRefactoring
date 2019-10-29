14b4200a00af06b66730af8f48d06775434bdda1
dung-kieu <Velvet99>
2019-10-29 09:06:21 +0700

    Clean code.

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,16 +1,21 @@
 public class Calculator {
 
-    public static int calculate(int a, int b, char o) {
-        switch (o) {
-            case '+':
-                return a + b;
-            case '-':
-                return a - b;
-            case '*':
-                return a * b;
-            case '/':
-                if (b != 0)
-                    return a / b;
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+    public static int calculate(int firstOperand, int SecondOperand, char operator) {
+        switch (operator) {
+            case ADDITION:
+                return firstOperand + SecondOperand;
+            case SUBTRACTION:
+                return firstOperand - SecondOperand;
+            case MULTIPLICATION:
+                return firstOperand * SecondOperand;
+            case DIVISION:
+                if (SecondOperand != 0)
+                    return firstOperand / SecondOperand;
                 else
                     throw new RuntimeException("Can not divide by 0");
             default:
